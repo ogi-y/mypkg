54c5864d399563f9ccd4bb669c0f8231e9a62f1e
Yoshihiro Ogishima <shigiom1231@gmail.com>
2022-12-28 20:08:10 +0900

    update

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -4,15 +4,19 @@ from color_msgs.msg import Color
 
 class Talker():
    def __init__(self, node):
-      self.pub = node.create_publisher(Color, "person", 10)
+      self.pub = node.create_publisher(Color, "color", 10)
       self.n = 0
       node.create_timer(0.5, self.cb)
 
    def cb(self):
-      msg = Int16()
-      msg.data = self.n
+      global n
+      msg = Color()
+      msg.red = 0
+      msg.green = 0
+      msg.blue = 0
+      msg.color_code = "000000"
       self.pub.publish(msg)
-      self.n += 1
+      n += 1
 
 def main():
    rclpy.init()
