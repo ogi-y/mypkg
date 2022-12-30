05c45db4efec28d6133f4a5b7c0447a9fb808612
Yoshihiro Ogishima <shigiom1231@gmail.com>
2022-12-30 19:38:37 +0900

    update

--- test/test.bash
+++ test/test.bash
@@ -5,10 +5,6 @@ dir=~
 
 export CMAKE_PREFIX_PATH=/opt/ros/humble/share/ament_cmake/
 cd $dir/ros2_ws
-ls ./src/ -l
-ls ./src/color_msgs/ -l
-ls ./src/mypkg/ -l
-cat ./src/color_msgs/package.xml
 colcon build
 source $dir/.bashrc
 timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
