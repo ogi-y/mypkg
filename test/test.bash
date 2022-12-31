256b44413defe7ea200845fc0a17351b45a98f1a
Yoshihiro Ogishima <shigiom1231@gmail.com>
2022-12-31 15:48:18 +0900

    update

--- test/test.bash
+++ test/test.bash
@@ -12,10 +12,11 @@ res=0
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-export CMAKE_PREFIX_PATH=/opt/ros/humble/share/ament_cmake/ & /opt/ros/humble/share/
+export CMAKE_PREFIX_PATH=/opt/ros/humble/share/ament_cmake/
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+ros2 pkg list
 timeout 3 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log |
 grep 'Listen.*Red:.*Green:.*Blue:.*色' || ng ${LINENO}
